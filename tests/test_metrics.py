0dcfeb3e2b3820e0c1afba3ab618c07d3f41eb9a
Toni Sissala <toni.sissala@tuni.fi>
2023-11-24 14:18:23 +0200

    63 count metrics for publishers counts and publishers counts without deleted using new schema (#64)
    Count metrics using Study._direct_base_url
    
    Use cdcagg_common 0.6.0 tag in requirements.txt. Require cdcagg_common 0.6.0 or newer in setup.py.
    
    Bump version to 0.7.0.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -40,7 +40,7 @@ class TestMetricsEndpoint(CDCAggOAIHTTPTestBase):
         )
 
     def test_calls_query_count_four_times_if_there_is_single_base_url(self):
-        self._mock_query_distinct.return_value = {"_provenance.base_url": ["some.base.url"]}
+        self._mock_query_distinct.return_value = {"_direct_base_url": ["some.base.url"]}
         self._mock_query_count.return_value = 1
         self.fetch("/metrics")
         self.assertEqual(self._mock_query_count.call_count, 4)
@@ -51,29 +51,12 @@ class TestMetricsEndpoint(CDCAggOAIHTTPTestBase):
                     Study,
                     _filter={Study._metadata.attr_status: {"$ne": "deleted"}},
                 ),
-                mock.call(
-                    Study,
-                    _filter={
-                        Study._provenance: {
-                            "$elemMatch": {
-                                Study._provenance.attr_base_url: "some.base.url",
-                                Study._provenance.attr_direct: True,
-                            }
-                        }
-                    },
-                ),
+                mock.call(Study, _filter={Study._direct_base_url: "some.base.url"}),
                 mock.call(
                     Study,
                     _filter={
                         "$and": [
-                            {
-                                Study._provenance: {
-                                    "$elemMatch": {
-                                        Study._provenance.attr_base_url: "some.base.url",
-                                        Study._provenance.attr_direct: True,
-                                    }
-                                }
-                            },
+                            {Study._direct_base_url: "some.base.url"},
                             {Study._metadata.attr_status: {"$ne": "deleted"}},
                         ]
                     },
@@ -84,7 +67,7 @@ class TestMetricsEndpoint(CDCAggOAIHTTPTestBase):
 
     def test_calls_query_count_eight_times_if_there_is_three_base_urls(self):
         self._mock_query_distinct.return_value = {
-            "_provenance.base_url": ["some.base.url", "another.base.url", "third.base.url"]
+            "_direct_base_url": ["some.base.url", "another.base.url", "third.base.url"]
         }
         self._mock_query_count.return_value = 1
         self.fetch("/metrics")
@@ -99,22 +82,22 @@ class TestMetricsEndpoint(CDCAggOAIHTTPTestBase):
         self.assertIn("records_total_without_deleted 2.0", self._fetch_resp_body_lines())
 
     def test_returns_publishers_total(self):
-        self._mock_query_distinct.return_value = {"_provenance.base_url": ["some.base.url"]}
+        self._mock_query_distinct.return_value = {"_direct_base_url": ["some.base.url"]}
         self._mock_query_count.return_value = 1
         self.assertIn("publishers_total 1.0", self._fetch_resp_body_lines())
 
     def test_returns_publishers_total_is_zero_if_count_query_returns_0(self):
-        self._mock_query_distinct.return_value = {"_provenance.base_url": ["some.base.url"]}
+        self._mock_query_distinct.return_value = {"_direct_base_url": ["some.base.url"]}
         self._mock_query_count.side_effect = [1, 1, 0]
         self.assertIn("publishers_total 0.0", self._fetch_resp_body_lines())
 
     def test_returns_publishers_counts(self):
-        self._mock_query_distinct.return_value = {"_provenance.base_url": ["some.base.url"]}
+        self._mock_query_distinct.return_value = {"_direct_base_url": ["some.base.url"]}
         self._mock_query_count.side_effect = [1, 1, 20, 0]
         self.assertIn('publishers_counts{publisher="some.base.url"} 20.0', self._fetch_resp_body_lines())
 
     def test_returns_publishers_counts_without_deleted(self):
-        self._mock_query_distinct.return_value = {"_provenance.base_url": ["some.base.url"]}
+        self._mock_query_distinct.return_value = {"_direct_base_url": ["some.base.url"]}
         self._mock_query_count.side_effect = [1, 1, 1, 30]
         self.assertIn(
             'publishers_counts_without_deleted{publisher="some.base.url"} 30.0', self._fetch_resp_body_lines()
